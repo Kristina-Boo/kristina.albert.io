212670f20572bf1c94edd6c4120982e7ebff41f2
Your Name <you@example.com>
2024-12-26 00:58:15 +0300

    hw2 js5

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-let a = 10;
-let b = 2;
-/*let sum = a + b;
-alert(sum)*/
-
-/*let difference = a - b;
-alert(difference);*/
-
-/*let product = a * b;
-alert(product);*/
-
-let quotient=a/b;
-alert(quotient);
+let rezult = 2 ** 5;
+alert(rezult);
