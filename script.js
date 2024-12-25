d4a3d27d37ec2300669875a7d6f437aa89002b2d
Your Name <you@example.com>
2024-12-26 00:52:28 +0300

    hw2 js4

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const creatorJs = 'Brendan Eich';
-alert(creatorJs);
+let a = 10;
+let b = 2;
+/*let sum = a + b;
+alert(sum)*/
+
+/*let difference = a - b;
+alert(difference);*/
+
+/*let product = a * b;
+alert(product);*/
+
+let quotient=a/b;
+alert(quotient);
