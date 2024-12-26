e424c995fd1476a090606b48ad73324f44212f26
Your Name <you@example.com>
2024-12-26 14:43:10 +0300

    hw2 js additional task

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 /*let a = 9;
 let b = 2;
 let rezult = a % b;
-alert(rezult);
+alert(rezult);*/
 
-let num = 1;
+/*let num = 1;
 num += 5;
 num -= 3;
 num *= 7;
 num /= 3;
 num++;
 num--;
-alert(num);
+alert(num);*/
 
-
-let age = Number (prompt('Сколько вам лет?'));
+/*let age = Number (prompt('Сколько вам лет?'));
 alert(age);
 
 const user = {
@@ -22,5 +21,18 @@ const user = {
     isAdmin: Boolean(1)
 }*/
 
-let name = prompt("Как вас зовут?");
+/*let name = prompt("Как вас зовут?");
 alert(`Привет, ${name}!`);
+
+const name = prompt('Введите ваше имя:');
+alert(`Привет, ${name}!`);*/
+
+let number = Number(prompt('Напиши любое число'));
+let rezult = number * 2;
+//alert(`Умножаем на 2 и получаем ${rezult}`);
+let rezult1 = rezult + 10;
+alert(`Прибавляем к ${rezult} 10 и получаем ${rezult1}`);
+//let rezult2 = rezult1 / 2;
+//alert(`Делим на 2 и получаем ${rezult2}`);
+//let rezult3 = rezult2 - number;
+//alert(`Из ${rezult2} вычитаем загаданное число и получается ${rezult3}`);
