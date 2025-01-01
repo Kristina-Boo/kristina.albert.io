8bad2426aaec9a409e35f8feb6aa43d0f8956152
Your Name <you@example.com>
2025-01-02 01:49:13 +0300

    add 4 task js and mobile position

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-/*let a = 9;
+let a = 20;
+alert(a);
+const date = 2007;
+alert('Год выхода первого iPhone - '+ date);
+const creatorJs = 'Brendan Eich';
+alert('Создатель JS - '+ creatorJs);
+let a = 10;
+let b = 2;
+let sum = a + b;
+alert  ('сумма A и B: '+ sum);
+let multi = a * b;
+alert  ('Результат умножения A и B: '+ multi);
+let division = a / b;
+alert  ('Результат деления A на B: '+ division);
+let subtraction = a - b;
+alert  ('Результат вычитания B от A: '+ subtraction);
+let rezult = 2 ** 5;
+alert(rezult);
+let a = 9;
 let b = 2;
 let rezult = a % b;
-alert(rezult);*/
-
-/*let num = 1;
+alert('Остаток от деления A на B: '+ rezult);
+let num = 1;
 num += 5;
 num -= 3;
 num *= 7;
 num /= 3;
 num++;
 num--;
-alert(num);*/
-
-/*let age = Number (prompt('Сколько вам лет?'));
-alert(age);
-
+alert(num);
+let age = Number (prompt('Сколько вам лет?'));
+alert('Количество полных лет - ' + age);
 const user = {
     name: String('Вася'),
     age: Number(35),
-    isAdmin: Boolean(1)
-}*/
-
-/*let name = prompt("Как вас зовут?");
+    isAdmin: Boolean(1) 
+}
+const name = prompt("Как вас зовут?");
 alert(`Привет, ${name}!`);
-
-const name = prompt('Введите ваше имя:');
-alert(`Привет, ${name}!`);*/
-
 let number = Number(prompt('Напиши любое число'));
 let rezult = number * 2;
-//alert(`Умножаем на 2 и получаем ${rezult}`);
+alert(`Умножаем на 2 и получаем ${rezult}`);
 let rezult1 = rezult + 10;
 alert(`Прибавляем к ${rezult} 10 и получаем ${rezult1}`);
-//let rezult2 = rezult1 / 2;
-//alert(`Делим на 2 и получаем ${rezult2}`);
-//let rezult3 = rezult2 - number;
-//alert(`Из ${rezult2} вычитаем загаданное число и получается ${rezult3}`);
